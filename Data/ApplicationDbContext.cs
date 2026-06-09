7f0c0e767d1c87b6ffc1d2fe2b3b1833f161ca27
haleemath-ashira <ashiraadam895@gmail.com>
2026-06-09 16:41:11 +0400

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JokesWebApp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    public DbSet<JokesWebApp.Models.Joke> Joke { get; set; } = default!;
+    }
+}
